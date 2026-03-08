b4787900bf0edaf832e97ff1aeed5a612f8b003c
yangjiey2k <42866354+yangjiey2k@users.noreply.github.com>
2026-03-08 20:37:39 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -6,18 +6,15 @@ UPDATE_PACKAGE() {
     local PKG_REPO=$2
     local PKG_BRANCH=$3
     local PKG_SPECIAL=$4
-    local PKG_LIST=("$PKG_NAME" $5) # 第5个参数为自定义名称列表
+    local PKG_LIST=("$PKG_NAME" $5)
     local REPO_NAME=${PKG_REPO#*/}
 
     echo " "
 
-    # 删除本地可能存在的不同名称的软件包
     for NAME in "${PKG_LIST[@]}"; do
-        # 查找匹配的目录
         echo "Search directory: $NAME"
         local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" 2>/dev/null)
 
-        # 删除找到的目录
         if [ -n "$FOUND_DIRS" ]; then
             while read -r DIR; do
                 rm -rf "$DIR"
@@ -28,10 +25,8 @@ UPDATE_PACKAGE() {
         fi
     done
 
-    # 克隆 GitHub 仓库
     git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
 
-    # 处理克隆的仓库
     if [[ "$PKG_SPECIAL" == "pkg" ]]; then
         find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
         rm -rf ./$REPO_NAME/
@@ -39,12 +34,6 @@ UPDATE_PACKAGE() {
         mv -f $REPO_NAME $PKG_NAME
     fi
 }
-
-# 调用示例
-# UPDATE_PACKAGE "OpenAppFilter" "destan19/OpenAppFilter" "master" "" "custom_name1 custom_name2"
-# UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf"
-# 这样会把原有的open-app-filter，luci-app-appfilter，oaf相关组件删除，不会出现coremark错误。
-# UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 
 UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
@@ -88,5 +77,4 @@ UPDATE_VERSION() {
     done
 }
 
-#UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
 UPDATE_VERSION "sing-box"
